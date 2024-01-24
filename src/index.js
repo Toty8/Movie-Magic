7477b5a0ec64516867a4afc4165e3ef0f2207555
Toty8 <80042351+Toty8@users.noreply.github.com>
2024-01-24 21:30:18 +0200

    Setup express

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log('Work');
+const express = require('express');
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.send('Work');
+});
+
+app.listen(port, () => console.log(`Server is working on port ${port}...`));
