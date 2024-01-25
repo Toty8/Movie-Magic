46b7e4242e989ada094d3a0c28bdf1effdc76c4c
Toty8 <80042351+Toty8@users.noreply.github.com>
2024-01-25 21:09:23 +0200

    Show dynamic movies on search page

--- src/controllers/movieController.js
+++ src/controllers/movieController.js
@@ -18,11 +18,12 @@ router.post('/movie/create', (req, res) => {
 router.get('/movie/details/:id', (req, res) => {
     const movieId = req.params.id;
     const movie = movieService.getById(movieId);
-
-    movie.rating = new Array(Number(movie.rating)).fill(true);
     
-    res.render('details', { movie });
+    if(!isNaN(movie.rating)){
+        movie.rating = new Array(Number(movie.rating)).fill(true);
+    }    
 
+    res.render('details', { movie });
 });
 
 module.exports = router;
