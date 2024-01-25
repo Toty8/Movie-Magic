bf535a1ee704423a47251c5076deb910428cc639
Toty8 <80042351+Toty8@users.noreply.github.com>
2024-01-25 15:28:44 +0200

    Create movie action and service

--- src/controllers/movieController.js
+++ src/controllers/movieController.js
@@ -1,7 +1,18 @@
 const router = require('express').Router();
 
-router.get('/movie/create', (req, res) =>{
+const movieService = require('../services/movieService'); 
+
+router.get('/movie/create', (req, res) => {
     res.render('create');
+});
+
+router.post('/movie/create', (req, res) => {
+
+    const newMovie = req.body;
+
+    movieService.create(newMovie);
+    
+    res.redirect('/');
 });
 
 module.exports = router;
