683c16cd70b01ed2da7c12f4c184e4d7b8aa986c
Toty8 <80042351+Toty8@users.noreply.github.com>
2024-01-25 15:37:08 +0200

    Add hardcoded movie

--- src/services/movieService.js
+++ src/services/movieService.js
@@ -1,5 +1,14 @@
-const movies = [];
+const movies = [{
+    title: 'TestTitle',
+    genre: 'TestGenre',
+    director: 'TestDirector',
+    date: '2024',
+    poster: './img/home-alone.jpeg',
+    rating: '3',
+    description: 'TestDescription'
+  }];
 
 exports.create = (movieData) => {
     movies.push(movieData);
+    console.log(movies);
 }
