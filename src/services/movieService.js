737d8fcd3d2ca50a7956a3dbb7ae8e2a4d5dccd5
Toty8 <80042351+Toty8@users.noreply.github.com>
2024-01-25 22:00:11 +0200

    Add filtration on the search page

--- src/services/movieService.js
+++ src/services/movieService.js
@@ -21,3 +21,19 @@ exports.getAll = () => {
 exports.getById = (movieId) => {
     return movies.find(m => m._id === Number(movieId));
 }
+
+exports.search = (title, genre, year) => {
+    let result = movies.slice();
+
+    if(title){
+        result = result.filter(m => m.title.toLowerCase().includes(title.toLowerCase()));
+    }
+    if(genre){
+        result = result.filter(m => m.genre.toLowerCase() === genre.toLowerCase());
+    }
+    if(year){
+        result = result.filter(m => m.date === year);
+    }
+
+    return result;
+}
