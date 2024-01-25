cdbc3a9756c0bfd5115b930412252f064d0e4bd1
Toty8 <80042351+Toty8@users.noreply.github.com>
2024-01-25 15:28:07 +0200

    Add body parser

--- src/config/configExpress.js
+++ src/config/configExpress.js
@@ -3,6 +3,7 @@ const path = require('path');
 
 function configExpress(app){
     app.use(express.static(path.resolve('src/static')));
+    app.use(express.urlencoded({ extended: false }));
 
     return app;
 }
