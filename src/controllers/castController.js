b65610d271388bb20f784e731c05c47ad73e5218
Toty8 <80042351+Toty8@users.noreply.github.com>
2024-01-28 11:29:18 +0200

    Add post action for cast create

--- src/controllers/castController.js
+++ src/controllers/castController.js
@@ -4,4 +4,16 @@ router.get('/create', (req, res) => {
     res.render('cast/create');
 });
 
+router.post('/create', (req, res) => {
+    const newCast = req.body;
+
+    try{
+        res.redirect('/');
+    }
+    catch(err){
+        console.log(err.message);
+        res.redirect('/create');
+    }
+});
+
 module.exports = router;
