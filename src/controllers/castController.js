23ba11b271cd741e615e521d8058b495212d808d
Toty8 <80042351+Toty8@users.noreply.github.com>
2024-01-28 12:06:08 +0200

    Save cast to db

--- src/controllers/castController.js
+++ src/controllers/castController.js
@@ -1,18 +1,21 @@
 const router = require('express').Router();
 
+const castService = require('../services/castService');
+
 router.get('/create', (req, res) => {
     res.render('cast/create');
 });
 
-router.post('/create', (req, res) => {
+router.post('/create', async (req, res) => {
     const newCast = req.body;
 
     try{
+        await castService.create(newCast);
         res.redirect('/');
     }
     catch(err){
         console.log(err.message);
-        res.redirect('/create');
+        res.redirect('/cast/create');
     }
 });
 
